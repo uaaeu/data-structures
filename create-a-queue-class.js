de409e47772827b83bd78f7b2a702932c1805e1f
uaaeu <yurtmusa@gmail.com>
2020-08-20 23:07:49 +0300

    create a queue class

---
+++ create-a-queue-class.js
@@ -0,0 +1,16 @@
+function Queue() {
+  let collection = [];
+  this.print = () => {
+    console.log(collection);
+  };
+
+  this.enqueue = (val) => collection.push(val);
+
+  this.dequeue = () => collection.shift();
+
+  this.front = () => collection[0];
+
+  this.size = () => collection.length;
+
+  this.isEmpty = () => collection.length === 0;
+}
