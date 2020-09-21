1643a57af40e1d4b887f6c22565d300938e01246
uaaeu <yurtmusa@gmail.com>
2020-09-21 11:33:30 +0300

    perform-a-union-on-two-sets

---
+++ perform-a-union-on-two-sets.js
@@ -0,0 +1,50 @@
+class Set {
+  constructor() {
+    // This will hold the set
+    this.dictionary = {};
+    this.length = 0;
+  }
+  // this method will check for the presence of an element and return true or false
+  has(element) {
+    return this.dictionary[element] !== undefined;
+  }
+  // this method will return all the values in the set
+  values() {
+    return Object.keys(this.dictionary);
+  }
+  // this method will add an element to the set
+  add(element) {
+    if (!this.has(element)) {
+      this.dictionary[element] = true;
+      this.length++;
+      return true;
+    }
+
+    return false;
+  }
+  // this method will remove an element from a set
+  remove(element) {
+    if (this.has(element)) {
+      delete this.dictionary[element];
+      this.length--;
+      return true;
+    }
+
+    return false;
+  }
+  // this method will return the size of the set
+  size() {
+    return this.length;
+  }
+  // Perform a Union on Two Sets
+  union(set) {
+    const newSet = new Set();
+    this.values().forEach(x => {
+      newSet.add(x);
+    })
+    set.values().forEach(x => {
+      newSet.add(x);
+    })
+    return newSet;
+  }
+}
