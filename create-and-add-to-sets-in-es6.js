4f78c6c3f58ccc019d49bf1f7b6ae6a1f11394e9
uaaeu <yurtmusa@gmail.com>
2020-09-22 10:28:56 +0300

    create-and-add-to-sets-in-es6

---
+++ create-and-add-to-sets-in-es6.js
@@ -0,0 +1,10 @@
+function checkSet() {
+  var set = new Set([1, 2, 3, 3, 2, 1, 2, 3, 1]);
+  // Create and Add to Sets in ES6
+  set.add('Taco');
+  set.add('Cat');
+  set.add('Awesome');
+  console.log(Array.from(set));
+  return set;
+}
+checkSet();
